866a166d36ac132a1964ebe8f909b3d0bff23f03
Esquetta <esquetta@gmail.com>
2023-07-09 14:41:54 +0300

    Add project files.

---
+++ FanoutExchangeConsumer/FanoutExchangeConsumer/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory connectionFactory = new ConnectionFactory();
+connectionFactory.Uri = new Uri("amqps://nmlgtsid:Hl-ZajX0FHO3eG52BQnrPKc8aQpkYTsd@sparrow.rmq.cloudamqp.com/nmlgtsid");
+
+using(IConnection connection=connectionFactory.CreateConnection())
+using(IModel channel=connection.CreateModel())
+{
+    channel.ExchangeDeclare("iskuyrugu", ExchangeType.Fanout);
+
+    string queueName = channel.QueueDeclare().QueueName;
+    channel.QueueBind(queueName, "iskuyrugu", "");
+
+    channel.BasicQos(0, 1, false);
+    EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+    channel.BasicConsume(queueName, false, consumer);
+
+    consumer.Received += (sender, e) =>
+    {
+        Thread.Sleep(1000);
+        channel.BasicAck(e.DeliveryTag, false);
+        Console.WriteLine(Encoding.UTF8.GetString(e.Body.ToArray())+" alındı");
+    };
+    Console.Read();
+
+}
---
+++ FanoutExchangePublisher/FanoutExchangePublisher/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory connectionFactory = new ConnectionFactory();
+connectionFactory.Uri = new Uri("amqps://nmlgtsid:Hl-ZajX0FHO3eG52BQnrPKc8aQpkYTsd@sparrow.rmq.cloudamqp.com/nmlgtsid");
+
+using(IConnection connection=connectionFactory.CreateConnection())
+using(IModel channel=connection.CreateModel())
+{
+    channel.ExchangeDeclare("iskuyrugu", ExchangeType.Fanout);
+	for (int i = 0; i <= 100; i++)
+	{
+		byte[] byteMessage = Encoding.UTF8.GetBytes($"is {i}");
+
+		IBasicProperties basicProperties = channel.CreateBasicProperties();
+		basicProperties.Persistent = true;
+
+		channel.BasicPublish("iskuyrugu", routingKey: "", basicProperties: basicProperties, body: byteMessage);
+	}
+}
